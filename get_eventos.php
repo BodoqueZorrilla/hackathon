cade010d5ecf5a54ccf4e9c68cf3c19abd78b208
Sergio Zorrilla <sergio.zorrilla@bsdenterprise.com>
2018-08-18 11:24:11 -0500

    ya tu sabe 22

---
+++ get_eventos.php
@@ -0,0 +1,31 @@
+<?php
+
+
+require_once("modelo.php");
+
+
+$id_ruta = $_REQUEST['id_ruta'];
+$modelo=new Modelo();
+
+$getCamiones=$modelo->consulta("CALL get_evento($id_ruta);");
+
+if(count($getCamiones) > 0){
+  $response["eventos"] = array();
+  foreach ($getCamiones as $key => $value) {
+    $rutas = array();
+    $rutas["id"] = (int)$value['id'];
+    $rutas["coordenadas"] = $value['coordenadas'];
+    $rutas["abrio_tapa"] = $value['abrio_tapa'];
+    $rutas["fecha"] = $value['hora'];
+    array_push($response["eventos"], $rutas);
+  }
+  $response["success"] = 1;
+  echo json_encode($response);
+}else{
+  $response["success"] = 0;
+  echo json_encode($response);
+}
+
+
+
+ ?>
