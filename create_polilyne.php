18bb016fad8e7de4e43fb402419338f9ab6f45c1
Sergio Zorrilla <sergio.zorrilla@bsdenterprise.com>
2018-08-18 08:42:52 -0500

    ya tu sabe 17

--- create_polilyne.php
+++ create_polilyne.php
@@ -4,7 +4,8 @@ require_once("modelo.php");
 $id_ruta = $_REQUEST['id_ruta'];
 $coordenadas = $_REQUEST['coordenadas'];
 $modelo_add=new Modelo();
-$add_pedido=$modelo_add->consulta("CALL ADD_POLYLINES(".$id_ruta.",'".$coordenadas."');");
+echo "CALL ADD_POLYLINES('".$id_ruta."','".$coordenadas."');";
+$add_pedido=$modelo_add->consulta("CALL ADD_POLYLINES('".$id_ruta."','".$coordenadas."');");
 
 $response["success"] = 1;
 echo json_encode($response);
