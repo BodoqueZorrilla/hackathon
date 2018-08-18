217a69097d9384dd30d31ea019867d01d3fb4d54
Sergio Zorrilla <sergio.zorrilla@bsdenterprise.com>
2018-08-18 05:49:50 -0500

    ya tu sabe 11

--- create_route.php
+++ create_route.php
@@ -1,9 +1,9 @@
 <?php
 require_once("modelo.php");
 $response["success"] = 1;
-$parametros = $_REQUEST['parametros'];
-$json_par = json_decode($parametros, true);
-echo json_encode($json_par);
+/*$parametros = $_REQUEST['parametros'];
+$json_par = json_decode($parametros, true);*/
+echo json_encode($response);
 /*$parametros = $_REQUEST['parametros'];
 
 $json_par = json_decode($parametros, true);
