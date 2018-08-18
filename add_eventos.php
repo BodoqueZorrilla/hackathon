e096ad1923002621f882ae5fb6eb2a0a816492e8
Sergio Zorrilla <sergio.zorrilla@bsdenterprise.com>
2018-08-18 09:58:04 -0500

    ya tu sabe 18

--- add_eventos.php
+++ add_eventos.php
@@ -3,10 +3,11 @@
 require_once("modelo.php");
 $id_ruta = $_REQUEST['id_ruta'];
 $coordenadas = $_REQUEST['coordenadas'];
+$abrio_tapa = $_REQUEST['abrio_tapa'];
+$hora = $_REQUEST['hora'];
 $modelo_add=new Modelo();
-$add_pedido=$modelo_add->consulta("CALL ADD_POLYLINES('".$id_ruta."','".$coordenadas."');");
+$add_pedido=$modelo_add->consulta("CALL add_evento('".$id_ruta."','".$coordenadas."','".$abrio_tapa."', '".$hora."');");
 $response["success"] = 1;
 echo json_encode($response);
-
 
  ?>
